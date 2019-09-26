cc5637c0d2732a60e0ba5681d34372bb6cc5adf5
Ian Lenehan <ianlenehan@gmail.com>
2019-09-26 21:48:04 +1000

    upgrade

--- components/AveragesCard.js
+++ components/AveragesCard.js
@@ -1,82 +1,100 @@
-import React from 'react';
-import { View, StyleSheet, Platform, Text, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
-import numeral from 'numeral'
-import Colours from '../constants/Colours'
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Platform,
+  Text,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
+import numeral from "numeral";
+import Colours from "../constants/Colours";
 
 export default class AveragesCard extends React.Component {
   state = {
     showStats: false,
-    data: { players: [] },
-  }
+    data: { players: [] }
+  };
 
   componentWillReceiveProps(nextProps) {
-    this.setState({ data: nextProps.data })
+    this.setState({ data: nextProps.data });
   }
 
   onPress = () => {
     if (this.props.allData) {
-      this.setState({ showStats: !this.state.showStats })
+      this.setState({ showStats: !this.state.showStats });
     }
-  }
+  };
 
   _renderStats() {
     if (this.state.showStats) {
-      console.log(this.props.allData);
-      const { allData } = this.props
+      const { allData } = this.props;
       return allData.map((item, i) => {
-        const percentage = numeral(item.avg).format('0%')
+        const percentage = numeral(item.avg).format("0%");
         return (
-          <View key={i} style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-            <Text style={{ textAlign: 'right', flex: 1 }}>{item.player.nick_name}</Text>
-            <Text style={{ fontWeight: 'bold', textAlign: 'left', flex: 1, marginLeft: 10 }}>{percentage}</Text>
+          <View
+            key={i}
+            style={{ flexDirection: "row", justifyContent: "space-around" }}
+          >
+            <Text style={{ textAlign: "right", flex: 1 }}>
+              {item.player.nick_name}
+            </Text>
+            <Text
+              style={{
+                fontWeight: "bold",
+                textAlign: "left",
+                flex: 1,
+                marginLeft: 10
+              }}
+            >
+              {percentage}
+            </Text>
           </View>
-        )
-      })
+        );
+      });
     }
   }
 
   _subtitle() {
     if (this.props.subtitle) {
-      const percentage = numeral(this.props.subtitle).format('0%')
+      const percentage = numeral(this.props.subtitle).format("0%");
       return (
         <View style={styles.circle}>
           <Text style={styles.subtitle}>{percentage}</Text>
         </View>
-      )
+      );
     }
   }
 
   _renderNameAndImage() {
     if (this.props.data) {
-      const { data: player } = this.props
+      const { data: player } = this.props;
       return (
-        <View style={{ alignItems: 'center' }}>
-          <Image
-            source={{uri: player.avatar_url}}
-            style={styles.image}
-          />
-          <View style={{ alignItems: 'center', flexDirection: 'row', marginTop: 5 }}>
+        <View style={{ alignItems: "center" }}>
+          <Image source={{ uri: player.avatar_url }} style={styles.image} />
+          <View
+            style={{ alignItems: "center", flexDirection: "row", marginTop: 5 }}
+          >
             <Text style={styles.nickName}>{player.nick_name}</Text>
             {this._subtitle()}
           </View>
           {this._renderStats()}
         </View>
-      )
+      );
     }
-    return <ActivityIndicator/>
+    return <ActivityIndicator />;
   }
 
   render() {
     return (
       <TouchableOpacity onPress={this.onPress}>
         <View style={styles.container}>
-          <Text style={styles.cardTitle}>
-            {this.props.title}
-          </Text>
+          <Text style={styles.cardTitle}>{this.props.title}</Text>
           {this._renderNameAndImage()}
         </View>
       </TouchableOpacity>
-    )
+    );
   }
 }
 
@@ -84,40 +102,40 @@ const styles = StyleSheet.create({
   container: {
     ...Platform.select({
       ios: {
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOffset: { height: -3 },
         shadowOpacity: 0.1,
-        shadowRadius: 3,
+        shadowRadius: 3
       },
       android: {
-        elevation: 20,
-      },
+        elevation: 20
+      }
     }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
     paddingVertical: 20,
-    marginBottom: 10,
+    marginBottom: 10
   },
   cardTitle: {
     fontSize: 28,
-    fontFamily: 'bangers',
-    color: 'rgb(255,0,128)',
-    padding: 5,
+    fontFamily: "bangers",
+    color: "rgb(255,0,128)",
+    padding: 5
   },
   image: {
     width: 80,
     height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
+    resizeMode: "contain",
+    marginTop: 3
   },
   nickName: {
     fontSize: 24,
-    fontFamily: 'gamja-flower'
+    fontFamily: "gamja-flower"
   },
   subtitle: {
     fontSize: 10,
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold"
   },
   circle: {
     width: 30,
@@ -125,7 +143,7 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     marginLeft: 10,
     backgroundColor: Colours.chairmansPink,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   ScrollView,
@@ -6,24 +6,24 @@ import {
   Text,
   View,
   ActivityIndicator,
-  RefreshControl,
-} from 'react-native';
-import { Button } from 'react-native-elements'
-import axios from 'axios'
-import _ from 'lodash'
+  RefreshControl
+} from "react-native";
+import { Button } from "react-native-elements";
+import axios from "axios";
+import _ from "lodash";
 
-import Card from '../components/Card';
-import AveragesCard from '../components/AveragesCard';
+import Card from "../components/Card";
+import AveragesCard from "../components/AveragesCard";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     headerTitle: (
-        <Image
-          style={{ width: 100, height: 30 }}
-          resizeMode='contain'
-          source={require('../assets/images/meow.png')}
-        />
-      ),
+      <Image
+        style={{ width: 100, height: 30 }}
+        resizeMode="contain"
+        source={require("../assets/images/meow.png")}
+      />
+    )
   };
 
   state = {
@@ -38,150 +38,164 @@ export default class HomeScreen extends React.Component {
     immunePlayer: null,
     refreshing: false,
     fromDate: "2019-01-01"
-  }
+  };
 
   componentDidMount() {
-    this.makeApiRequest()
+    this.makeApiRequest();
   }
 
   _onRefresh = async () => {
-    this.setState({ refreshing: true })
-    await this.makeApiRequest()
-    this.setState({ refreshing: false })
-  }
+    this.setState({ refreshing: true });
+    await this.makeApiRequest();
+    this.setState({ refreshing: false });
+  };
 
   async makeApiRequest() {
-    const { fromDate } = this.state
-    const res = await axios.get(`https://chairmans-api.herokuapp.com/results?from=${fromDate}`)
-    const { players, results, round_counts: roundCounts } = res.data
+    const { fromDate } = this.state;
+    const res = await axios.get(
+      `https://chairmans-api.herokuapp.com/results?from=${fromDate}`
+    );
+    const { players, results, round_counts: roundCounts } = res.data;
     if (results.length) {
-      this.getHatHolder(results, players)
-      this.getImmunePlayer(results, players)
-      this.getMostHats(results, players)
-      this.getMostWins(results, players)
-      this.getAllHats(results, players, roundCounts)
-      this.getAllWins(results, players, roundCounts)
+      this.getHatHolder(results, players);
+      this.getImmunePlayer(results, players);
+      this.getMostHats(results, players);
+      this.getMostWins(results, players);
+      this.getAllHats(results, players, roundCounts);
+      this.getAllWins(results, players, roundCounts);
     }
   }
 
   getHatHolder(results, players) {
-    const [lastRound] = results
-    const hatHolders = players.filter((player) => player.id === lastRound.loser)
-    this.setState({ hatHolder: hatHolders[0] })
+    const [lastRound] = results;
+    const hatHolders = players.filter(player => player.id === lastRound.loser);
+    this.setState({ hatHolder: hatHolders[0] });
   }
 
   getImmunePlayer(results, players) {
-    const [lastRound] = results
-    const immunePlayers = players.filter((player) => player.id === lastRound.winner)
-    this.setState({ immunePlayer: immunePlayers[0] })
+    const [lastRound] = results;
+    const immunePlayers = players.filter(
+      player => player.id === lastRound.winner
+    );
+    this.setState({ immunePlayer: immunePlayers[0] });
   }
 
   _resultsPerPlayer(playerId, rewards) {
     return rewards.reduce((acc, reward) => {
-      if (reward === playerId) acc += 1
-      return acc
-    }, 0)
+      if (reward === playerId) acc += 1;
+      return acc;
+    }, 0);
   }
 
   _initialCounts(players, rewards) {
-    return players.map((player) => {
-      const total = this._resultsPerPlayer(player.id, rewards)
-      return { player, total }
-    })
+    return players.map(player => {
+      const total = this._resultsPerPlayer(player.id, rewards);
+      return { player, total };
+    });
   }
 
   _sortedCounts(counts) {
-    return counts.sort(function (a, b) {
-      return Number(b.total) - Number(a.total)
-    })
+    return counts.sort(function(a, b) {
+      return Number(b.total) - Number(a.total);
+    });
   }
 
   _mostHatsOrLosses(counts) {
-    const sortedCounts = this._sortedCounts(counts)
-    const most = sortedCounts[0].total
-    const filtered = sortedCounts.filter((count) => Number(count.total) === most)
-    const players = filtered.map((x) => x.player)
-    return { most, players }
+    const sortedCounts = this._sortedCounts(counts);
+    const most = sortedCounts[0].total;
+    const filtered = sortedCounts.filter(count => Number(count.total) === most);
+    const players = filtered.map(x => x.player);
+    return { most, players };
   }
 
   getMostHats(results, players) {
-    const rewards = results.map((result) => result.loser)
-    const counts = this._initialCounts(players, rewards)
-    const mostHats = this._mostHatsOrLosses(counts)
-    this.setState({ mostHats })
+    const rewards = results.map(result => result.loser);
+    const counts = this._initialCounts(players, rewards);
+    const mostHats = this._mostHatsOrLosses(counts);
+    this.setState({ mostHats });
   }
 
   _allCounts(players, rewards) {
-    return players.map((player) => {
-      const total = this._resultsPerPlayer(player.id, rewards)
-      return { playerName: player.nick_name, total }
-    })
+    return players.map(player => {
+      const total = this._resultsPerPlayer(player.id, rewards);
+      return { playerName: player.nick_name, total };
+    });
   }
 
   _sortedAvgs(averages) {
-    return averages.sort(function (a, b) {
-      return Number(b.avg) - Number(a.avg)
-    })
+    return averages.sort(function(a, b) {
+      return Number(b.avg) - Number(a.avg);
+    });
   }
 
   _getHatAvgs(roundCounts, players, allHats) {
     return roundCounts.map(rounds => {
-      const [player] = Object.keys(rounds)
-      const [length] = Object.values(rounds)
+      const [player] = Object.keys(rounds);
+      const [length] = Object.values(rounds);
       const [hats] = allHats.filter(playerData => {
-        return playerData["playerName"] === player
-      })
+        return playerData["playerName"] === player;
+      });
       const [playerData] = players.filter(fullPlayer => {
-        return fullPlayer.nick_name === player
-      })
-      return {  player: playerData, avg: Number(hats.total) / length}
-    })
+        return fullPlayer.nick_name === player;
+      });
+      return { player: playerData, avg: Number(hats.total) / length };
+    });
   }
 
   getAllHats(results, players, roundCounts) {
-    const rewards = results.map((result) => result.loser)
-    const allHats = this._allCounts(players, rewards)
-    const hatAvgs = this._getHatAvgs(roundCounts, players, allHats)
-    const sortedHatAvgs = this._sortedAvgs(hatAvgs)
-    this.setState({ allHats, hatAvg: sortedHatAvgs })
+    const rewards = results.map(result => result.loser);
+    const allHats = this._allCounts(players, rewards);
+    const hatAvgs = this._getHatAvgs(roundCounts, players, allHats);
+    const sortedHatAvgs = this._sortedAvgs(hatAvgs);
+    this.setState({ allHats, hatAvg: sortedHatAvgs });
   }
 
   _getWinAvgs(roundCounts, players, allWins) {
     return roundCounts.map(rounds => {
-      const [player] = Object.keys(rounds)
-      const [length] = Object.values(rounds)
+      const [player] = Object.keys(rounds);
+      const [length] = Object.values(rounds);
       const [wins] = allWins.filter(playerData => {
-        return playerData["playerName"] === player
-      })
+        return playerData["playerName"] === player;
+      });
       const [playerData] = players.filter(fullPlayer => {
-        return fullPlayer.nick_name === player
-      })
-      return {  player: playerData, avg: Number(wins.total) / length}
-    })
+        return fullPlayer.nick_name === player;
+      });
+      return { player: playerData, avg: Number(wins.total) / length };
+    });
   }
 
   getAllWins(results, players, roundCounts) {
-    const rewards = results.map((result) => result.winner)
-    const allWins = this._allCounts(players, rewards)
-    const winAvgs = this._getWinAvgs(roundCounts, players, allWins)
-    const sortedWinAvgs = this._sortedAvgs(winAvgs)
-    this.setState({ allWins, winAvg: sortedWinAvgs })
+    const rewards = results.map(result => result.winner);
+    const allWins = this._allCounts(players, rewards);
+    const winAvgs = this._getWinAvgs(roundCounts, players, allWins);
+    const sortedWinAvgs = this._sortedAvgs(winAvgs);
+    this.setState({ allWins, winAvg: sortedWinAvgs });
   }
 
   getMostWins(results, players) {
-    const rewards = results.map((result) => result.winner)
-    const counts = this._initialCounts(players, rewards)
-    const mostWins = this._mostHatsOrLosses(counts)
-    this.setState({ mostWins })
+    const rewards = results.map(result => result.winner);
+    const counts = this._initialCounts(players, rewards);
+    const mostWins = this._mostHatsOrLosses(counts);
+    this.setState({ mostWins });
   }
 
-  filterByYear = async (fromDate) => {
-    await this.setState({ fromDate })
-    this.makeApiRequest()
-  }
+  filterByYear = async fromDate => {
+    await this.setState({ fromDate });
+    this.makeApiRequest();
+  };
 
   render() {
-    const { hatHolder, mostHats, mostWins, allHats, allWins, highestAverage, immunePlayer, hatAvg, winAvg } = this.state
+    const {
+      hatHolder,
+      mostHats,
+      mostWins,
+      allHats,
+      allWins,
+      highestAverage,
+      immunePlayer,
+      hatAvg,
+      winAvg
+    } = this.state;
     if (mostHats && mostWins && hatAvg && winAvg) {
       return (
         <ScrollView
@@ -192,96 +206,109 @@ export default class HomeScreen extends React.Component {
             />
           }
         >
-        <View style={{ display: "flex", flex: 1, flexDirection: "row", justifyContent: "center" }}>
-         <Button
-          title='2018' borderRadius={5} onPress={() => this.filterByYear('2018-01-01')} />
-          <Button
-          title='2019' borderRadius={5} onPress={() => this.filterByYear('2019-01-01')} />
-        </View>
+          <View
+            style={{
+              display: "flex",
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "center"
+            }}
+          >
+            <Button
+              title="2018"
+              borderRadius={5}
+              onPress={() => this.filterByYear("2018-01-01")}
+            />
+            <Button
+              title="2019"
+              borderRadius={5}
+              onPress={() => this.filterByYear("2019-01-01")}
+            />
+          </View>
           <Card
             data={hatHolder}
             navigation={this.props.navigation}
-            title='Current Hat Holder'
+            title="Current Hat Holder"
             singlePlayer
           />
           <Card
             data={immunePlayer}
             navigation={this.props.navigation}
-            title='Four Point Advantage'
+            title="Four Point Advantage"
             singlePlayer
           />
           <Card
             data={mostHats}
             allData={allHats}
             navigation={this.props.navigation}
-            title='Most Hats'
+            title="Most Hats"
             subtitle={mostHats.most}
           />
           <Card
             data={mostWins}
             allData={allWins}
             navigation={this.props.navigation}
-            title='Most Wins'
+            title="Most Wins"
             subtitle={mostWins.most}
           />
           <AveragesCard
             data={hatAvg[0].player}
             allData={hatAvg}
             navigation={this.props.navigation}
-            title='Highest Hat Average'
+            title="Highest Hat Average"
             subtitle={hatAvg[0].avg}
           />
           <AveragesCard
             data={winAvg[0].player}
             allData={winAvg}
             navigation={this.props.navigation}
-            title='Highest Win Average'
+            title="Highest Win Average"
             subtitle={winAvg[0].avg}
           />
         </ScrollView>
       );
     }
     return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <ActivityIndicator/>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   titleText: {
     marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
+    color: "rgba(0,0,0,0.4)",
     fontSize: 14,
     lineHeight: 19,
-    textAlign: 'center',
+    textAlign: "center"
   },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 30
   },
   header: {
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 20,
     padding: 10,
-    backgroundColor: '#ff0080',
-    height: 100,
+    backgroundColor: "#ff0080",
+    height: 100
   },
   tabBarInfoText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center"
   },
   hatOwnerContainer: {
     padding: 10,
     margin: 25,
     borderWidth: 5,
-    borderColor: '#ff0080',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: "#ff0080",
+    alignItems: "center",
+    justifyContent: "center"
   },
   hatImageStyle: {
     height: 150,
-    width: 150,
+    width: 150
   }
 });
